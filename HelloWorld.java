dc35d6de25063d082482aa36696f69cf971f0fef
Vinh Nguyen <vinhmaivy@gmail.com>
2020-11-01 15:02:56 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+    	int i;
+    	char c = 'a';    	
+        i = 5;
+        i++;        
+        System.out.println("Hello, World!");
+        System.out.println(i);
+        System.out.println(c);
+        
+    }
+}
