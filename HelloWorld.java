73870f763678f3ad2b92cc431cc07307d76bba64
Vinh Nguyen <vinhmaivy@gmail.com>
2020-11-01 15:59:44 -0600

    2020-11-01 16:00

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-    	int i;
-    	char c = 'a';    	
-        i = 5;
-        i++;        
         System.out.println("Hello, World!");
-        System.out.println(i);
-        System.out.println(c);        
     }
 }
