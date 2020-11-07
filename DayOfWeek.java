2b0d9017f7f4c8e6f4b3955855dc0c960986b0d0
vinhm <vinhm@pacificv>
2020-11-07 05:52:15 -0600

    2020-11-07 6:00

--- DayOfWeek.java
+++ DayOfWeek.java
@@ -8,42 +8,42 @@ public class DayOfWeek {
 		System.out.println("For " + month + "/" + day + "/" + year);
 		
 		// Take the last two digits of the year.
-		int workday =  year % 100;
-        System.out.println("Step 1: Take the last two digits of the year = " + workday);
+		int dayofweek =  year % 100;
+        System.out.println("Step 1: Take the last two digits of the year = " + dayofweek);
 
 		// Divide by 4, discarding any fraction.
-		workday = workday / 4;
-        System.out.println("Step 2: Divide by 4, discarding any fraction = " + workday);
+		dayofweek = dayofweek / 4;
+        System.out.println("Step 2: Divide by 4, discarding any fraction = " + dayofweek);
         
 		// Add the day of the month.
-		workday += day;
-        System.out.println("Step 3: Add the day of the month = " + workday);		
+		dayofweek += day;
+        System.out.println("Step 3: Add the day of the month = " + dayofweek);		
 		
 		// Add the month's key value: JFM AMJ JAS OND 144 025 036 146
 		int[] add_month = {1,4,4,0,2,5,0,3,6,1,4,6};
-		workday += add_month[month-1];
-        System.out.println("Step 4: Add the month's key value = " + workday);		
+		dayofweek += add_month[month-1];
+        System.out.println("Step 4: Add the month's key value = " + dayofweek);		
 		
 		// Subtract 1 for January or February of a leap year.
         //if (year % 4 == 0 && month <=2)
-        //	workday -= 1;
+        //	dayofweek -= 1;
         
 		// Add 0 for 1900's, 6 for 2000's, 4 for 1700's, 2 for 1800's; for other years, add or subtract multiples of 400.
-		workday += 6;
-        System.out.println("Step 5: Add 6 for 2000's = " + workday);		
+		dayofweek += 6;
+        System.out.println("Step 5: Add 6 for 2000's = " + dayofweek);		
 
 		// Add the last two digits of the year.
-        workday += year % 100;
-        System.out.println("Step 6: Add the last two digits of the year = " + workday);
+        dayofweek += year % 100;
+        System.out.println("Step 6: Add the last two digits of the year = " + dayofweek);
         
 		// Divide by 7 and take the remainder.
-		workday = workday % 7;
-        System.out.println("Step 7: Divide by 7 and take the remainder = " + workday);
+		dayofweek = dayofweek % 7;
+        System.out.println("Step 7: Divide by 7 and take the remainder = " + dayofweek);
 				
-        System.out.println("Day of week: " + workday);
+        System.out.println("Day of week: " + dayofweek);
         
 		String[] weekday = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        System.out.println("Day of week: " + weekday[workday-1]);
+        System.out.println("Day of week: " + weekday[dayofweek-1]);
 
 	}
 
